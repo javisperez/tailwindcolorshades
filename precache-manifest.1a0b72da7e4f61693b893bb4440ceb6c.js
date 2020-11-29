fc7c1a4d7ed90d207ab208292a017c2d3e1b9d99
javisperez <javisperez@gmail.com>
2020-11-29 21:39:03 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.1a0b72da7e4f61693b893bb4440ceb6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f910c68136d6652c4945",
+    "url": "/css/app.1aab2b95.css"
+  },
+  {
+    "revision": "f07db7b24e49448f2ee19a0903e92ac8",
+    "url": "/img/logo.f07db7b2.png"
+  },
+  {
+    "revision": "6cf0de052abf4737a71acabeccf81c21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f910c68136d6652c4945",
+    "url": "/js/app.aae1b8b3.js"
+  },
+  {
+    "revision": "80f5b98a08bd02518244",
+    "url": "/js/chunk-vendors.4c169de9.js"
+  },
+  {
+    "revision": "65bc7fa8f7c0d9acbc9d70774376404f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
