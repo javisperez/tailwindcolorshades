7a7286b008e0ead52d14bface8ca0240a00c7e07
Javis Perez <javisperez@gmail.com>
2023-06-05 15:26:12 -0400

    Use real world colors instead
    Co-Authored-By: tleish <1099993+tleish@users.noreply.github.com>

--- src/composables/colors.ts
+++ src/composables/colors.ts
@@ -1,5 +1,7 @@
 // @ts-ignore
 import colorNamer from "color-namer";
+// @ts-ignore
+import convert from "color-convert";
 
 export type Palette = {
   name: string;
@@ -8,76 +10,53 @@ export type Palette = {
   }
 };
 
-type Rgb = {
-  r: number;
-  g: number;
-  b: number;
-};
-
-function hexToRgb(hex: String): Rgb | null {
-  // @ts-ignore
-  const sanitizedHex = hex.replaceAll("##", "#");
-  const colorParts = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(
-    sanitizedHex
-  );
-
-  if (!colorParts) {
-    return null;
-  }
-
-  const [, r, g, b] = colorParts;
-
-  return {
-    r: parseInt(r, 16),
-    g: parseInt(g, 16),
-    b: parseInt(b, 16)
-  } as Rgb;
-}
-
-function rgbToHex(r: number, g: number, b: number): string {
-  const toHex = (c: number) => `0${c.toString(16)}`.slice(-2);
-  return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
-}
+const CMY_HUES = [180, 300, 60];
+const RGB_HUES = [360, 240, 120, 0];
 
 export function getTextColor(color: string): "#FFF" | "#333" {
-  const rgbColor = hexToRgb(color);
+  const rgbColor = convert.hex.rgb(color);
 
   if (!rgbColor) {
     return "#333";
   }
 
-  const { r, g, b } = rgbColor;
+  const [r, g, b] = rgbColor;
   const luma = 0.2126 * r + 0.7152 * g + 0.0722 * b;
 
   return luma < 120 ? "#FFF" : "#333";
 }
 
-function lighten(hex: string, intensity: number): string {
-  const color = hexToRgb(`#${hex}`);
+function hueShift(hues: Array<number>, hue: number, intensity: number) {
+  const closestHue = hues.sort((a, b) => (Math.abs(a - hue) - Math.abs(b - hue)))[0],
+    hueShift = closestHue - hue;
+  return Math.round(intensity * hueShift * 0.5);
+}
 
-  if (!color) {
+function lighten(hex: string, intensity: number): string {
+  if (!hex) {
     return "";
   }
 
-  const r = Math.round(color.r + (255 - color.r) * intensity);
-  const g = Math.round(color.g + (255 - color.g) * intensity);
-  const b = Math.round(color.b + (255 - color.b) * intensity);
+  const [h, s, v] = convert.hex.hsv(hex);
+  const hue = h + hueShift(CMY_HUES, h, intensity);
+  const saturation = s - Math.round(s * intensity);
+  const value = v + Math.round((100 - v) * intensity);
 
-  return rgbToHex(r, g, b);
+  return `#${convert.hsv.hex([hue, saturation, value])}`;
 }
 
 function darken(hex: string, intensity: number): string {
-  const color = hexToRgb(hex);
-
-  if (!color) {
+  if (!hex) {
     return "";
   }
 
-  const r = Math.round(color.r * intensity);
-  const g = Math.round(color.g * intensity);
-  const b = Math.round(color.b * intensity);
+  const inverseIntensity =  (1 - intensity);
+  const [h, s, v] = convert.hex.hsv(hex);
+  const hue = h + hueShift(RGB_HUES, h, inverseIntensity);
+  const saturation = s + Math.round((100 - s) * inverseIntensity);
+  const value = v - Math.round(v * inverseIntensity);
 
-  return rgbToHex(r, g, b);
+  return `#${convert.hsv.hex([hue, saturation, value])}`;
 }
 
 export function isValidHexColorCode(str: string) {
@@ -127,7 +106,7 @@ export default function (baseColor?: string): Palette | undefined {
     700: 0.75,
     800: 0.6,
     900: 0.45,
-    950: 0.25
+    950: 0.29
   };
 
   [50, 100, 200, 300, 400].forEach(level => {
