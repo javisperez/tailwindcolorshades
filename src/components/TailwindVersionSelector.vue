88c7f379661f45edc22bab4d96e4001966b9b61d
Javis V. Pérez <javisperez@gmail.com>
2025-04-09 21:30:06 -0400

    Add switch from v4 to v3
    Import needs rework

---
+++ src/components/TailwindVersionSelector.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref } from 'vue';
+
+const emit = defineEmits(['select'])
+
+const props = defineProps({
+  version: {
+    type: String,
+    default: 'v4'
+  }
+});
+
+const selectedVersion = ref(props.version || 'v4');
+
+function setVersion(version) {
+  selectedVersion.value = version;
+  emit('select', version);
+}
+</script>
+
+<template>
+  <div class="hidden lg:flex items-center">
+    <div class="flex rounded-md bg-gray-200">
+      <button
+        :class="{
+          'bg-primary-500 text-white': selectedVersion === 'v4',
+          'bg-transparent text-gray-700': selectedVersion !== 'v4'
+        }"
+        @click="setVersion('v4')"
+        class="px-3 py-1.5 text-sm font-medium rounded-l-md">
+        v4
+      </button>
+      <button
+        :class="{
+          'bg-primary-500 text-white': selectedVersion === 'v3',
+          'bg-transparent text-gray-700': selectedVersion !== 'v3'
+        }"
+        @click="setVersion('v3')"
+        class="px-3 py-1.5 text-sm font-medium rounded-r-md">
+        v3
+      </button>
+    </div>
+  </div>
+</template>
--- src/composables/source.ts
+++ src/composables/source.ts
@@ -1,12 +1,32 @@
 import type { Palette } from "./colors";
 import { computed } from "vue";
 
-export default function useTailwindTheme(palette: Palette) {
-  const themeVariables = computed(() =>
-    Object.entries(palette.colors)
-      .map(([shade, colorValue]) => `  --color-${palette.name.toLowerCase()}-${shade}: ${colorValue};`)
-      .join("\n")
-  );
+export default function useTailwindTheme(palette: Palette, isV4: boolean = true) {
+  // Source code for Tailwind CSS v4
+  const getV4Source = () => {
+    const themeVariables = computed(() =>
+      Object.entries(palette.colors)
+        .map(([shade, colorValue]) => `  --color-${palette.name.toLowerCase()}-${shade}: ${colorValue};`)
+        .join("\n")
+    );
 
-  return `@theme {\n${themeVariables.value}\n}`;
+    return `@theme {\n${themeVariables.value}\n}`;
+  }
+
+  // Source code for Tailwind CSS v3
+  const getV3Source = () => {
+    const shades = computed(() => Object.keys(palette.colors));
+
+    const shadesSource = computed(() =>
+      shades.value.map(
+        (shade: string) => `    '${shade}': '${palette.colors[Number(shade)]}'`
+      )
+    );
+
+    return `'${palette.name.toLowerCase()}': {
+${shadesSource.value.join(", \n")}
+}`;
+  }
+
+  return isV4 ? getV4Source() : getV3Source();
 }
